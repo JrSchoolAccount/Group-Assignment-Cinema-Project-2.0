c0c0e4ab5eec5c143182c4ca57ab1132e691d2c0
JrSchoolAccount <jesperx2000@gmail.com>
2024-01-30 14:58:52 +0100

    Changed to template literal

--- src/Reviews.js
+++ src/Reviews.js
@@ -3,14 +3,14 @@ import fetch from 'node-fetch';
 const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 
 export async function loadAllReviews() {
-  const res = await fetch(API_BASE + '/reviews');
+  const res = await fetch(`${API_BASE}/reviews`);
   const payload = await res.json();
 
   return payload;
 }
 
 export async function loadReview(id) {
-  const res = await fetch(API_BASE + '/reviews/' + id);
+  const res = await fetch(`${API_BASE}/reviews?filters[movie]=${id}`);
   const payload = await res.json();
 
   return payload;
