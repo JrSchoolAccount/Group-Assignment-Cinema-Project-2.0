1cfe13ac3e2bab4390f1ecf4576ecd6072de9d12
JrSchoolAccount <jesperx2000@gmail.com>
2024-01-31 21:43:38 +0100

    logic for the movieReviews route

---
+++ src/movieReviews.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
+
+export async function loadMovieReviews(id) {
+  const res = await fetch(
+    `${API_BASE}/reviews?filters[movie]=${id}&pagination[pagesize]=5&pagination[page]=${page}`
+  );
+  const payload = await res.json();
+
+  return payload;
+}
