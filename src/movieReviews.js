4e41d55eac8cf2d8ed104bb51d251c127fbfd417
JrSchoolAccount <jesperx2000@gmail.com>
2024-02-05 14:04:52 +0100

    corrected backend code

--- src/movieReviews.js
+++ src/movieReviews.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 
-export async function loadMovieReviews(id) {
+export async function loadMovieReviews(id, page) {
   const res = await fetch(
     `${API_BASE}/reviews?filters[movie]=${id}&pagination[pagesize]=5&pagination[page]=${page}`
   );
