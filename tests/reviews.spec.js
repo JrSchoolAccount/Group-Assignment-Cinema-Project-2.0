2f1862f67fce4acc1b92966ae3ce811389f70d27
JrSchoolAccount <jesperx2000@gmail.com>
2024-02-08 14:38:10 +0100

    Updated the test to test all movie pages

--- tests/reviews.spec.js
+++ tests/reviews.spec.js
@@ -2,9 +2,105 @@ import { jest } from '@jest/globals';
 import request from 'supertest';
 import app from '../src/app';
 
-describe('Pagination test', () => {
+describe('Pagination test for Pulp Fiction page', () => {
+  it('should return paginated reviews for a movie with pageSize: 5', async () => {
+    const movieId = 8;
+    const page = 1;
+
+    const res = await request(app)
+      .get(`/api/movies/${movieId}/reviews`)
+      .query({ page });
+
+    expect(res.status).toBe(200);
+    expect(res.body.reviews.pagination).toBeDefined();
+    expect(res.body.reviews.pagination.page).toBe(page);
+    expect(res.body.reviews.pagination.pageSize).toBe(5);
+  });
+});
+
+describe('Pagination test for Encanto page', () => {
   it('should return paginated reviews for a movie with pageSize: 5', async () => {
     const movieId = 2;
+    const page = 1;
+
+    const res = await request(app)
+      .get(`/api/movies/${movieId}/reviews`)
+      .query({ page });
+
+    expect(res.status).toBe(200);
+    expect(res.body.reviews.pagination).toBeDefined();
+    expect(res.body.reviews.pagination.page).toBe(page);
+    expect(res.body.reviews.pagination.pageSize).toBe(5);
+  });
+});
+
+describe('Pagination test for Isle of dogs page', () => {
+  it('should return paginated reviews for a movie with pageSize: 5', async () => {
+    const movieId = 1;
+    const page = 1;
+
+    const res = await request(app)
+      .get(`/api/movies/${movieId}/reviews`)
+      .query({ page });
+
+    expect(res.status).toBe(200);
+    expect(res.body.reviews.pagination).toBeDefined();
+    expect(res.body.reviews.pagination.page).toBe(page);
+    expect(res.body.reviews.pagination.pageSize).toBe(5);
+  });
+});
+
+describe('Pagination test for Min granne Totoro page', () => {
+  it('should return paginated reviews for a movie with pageSize: 5', async () => {
+    const movieId = 4;
+    const page = 1;
+
+    const res = await request(app)
+      .get(`/api/movies/${movieId}/reviews`)
+      .query({ page });
+
+    expect(res.status).toBe(200);
+    expect(res.body.reviews.pagination).toBeDefined();
+    expect(res.body.reviews.pagination.page).toBe(page);
+    expect(res.body.reviews.pagination.pageSize).toBe(5);
+  });
+});
+
+describe('Pagination test for The Shawshank Redemption page', () => {
+  it('should return paginated reviews for a movie with pageSize: 5', async () => {
+    const movieId = 3;
+    const page = 1;
+
+    const res = await request(app)
+      .get(`/api/movies/${movieId}/reviews`)
+      .query({ page });
+
+    expect(res.status).toBe(200);
+    expect(res.body.reviews.pagination).toBeDefined();
+    expect(res.body.reviews.pagination.page).toBe(page);
+    expect(res.body.reviews.pagination.pageSize).toBe(5);
+  });
+});
+
+describe('Pagination test for Forrest Gump page', () => {
+  it('should return paginated reviews for a movie with pageSize: 5', async () => {
+    const movieId = 6;
+    const page = 1;
+
+    const res = await request(app)
+      .get(`/api/movies/${movieId}/reviews`)
+      .query({ page });
+
+    expect(res.status).toBe(200);
+    expect(res.body.reviews.pagination).toBeDefined();
+    expect(res.body.reviews.pagination.page).toBe(page);
+    expect(res.body.reviews.pagination.pageSize).toBe(5);
+  });
+});
+
+describe('Pagination test for The Muppets page', () => {
+  it('should return paginated reviews for a movie with pageSize: 5', async () => {
+    const movieId = 5;
     const page = 1;
 
     const res = await request(app)
