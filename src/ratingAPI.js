428743792cded048c91556b8620b137a3cc9a09c
rl-86 <rl86@live.se>
2024-02-13 15:20:38 +0100

    cleanup

--- src/ratingAPI.js
+++ src/ratingAPI.js
@@ -13,15 +13,13 @@ export async function getMovieRating(id) {
     const ratings = payload.data.map((obj) => obj.attributes.rating);
     const totalRating = ratings.reduce((acc, curr) => acc + curr, 0);
     const averageRating = totalRating / ratings.length;
-    console.log('User Rating:', averageRating.toFixed(1));
     return averageRating.toFixed(1);
   } else {
     const getImdbId = await loadMovie(id);
     const imdbId = getImdbId.imdbId;
     const omdbRes = await fetch(`${omdbAPI}/?apikey=${API_KEY}&i=${imdbId}`);
     const omdbData = await omdbRes.json();
-    const imdbRating = parseFloat(omdbData.imdbRating);
-    console.log('IMDB Rating:', imdbRating.toFixed(1));
-    return imdbRating.toFixed(1);
+    const imdbRating = omdbData.imdbRating;
+    return imdbRating;
   }
 }
